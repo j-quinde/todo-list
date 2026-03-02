d5e287600d222c26932a32d72244d406ac8c58bd
Jossy Quinde <jqquindem@gmail.com>
2026-03-02 11:21:47 -0500

    comment: se agrega comentario en models.py

--- base/models.py
+++ base/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+# Mi tabla de tareas
 class Tarea(models.Model):
     usuario = models.ForeignKey(User,
                                 on_delete=models.CASCADE,
