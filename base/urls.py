e20c40696cdc46910952ff859451947a95733bf0
Jossy Quinde <jqquindem@gmail.com>
2026-03-06 14:50:34 -0500

    feat(auth): se implementa funcionalidad login/logout

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from . import views
-from .views import ListaTareas, CrearTarea, EditarTarea, eliminarTarea
-
+from .views import ListaTareas, CrearTarea, EditarTarea, eliminarTarea, Login
+from django.contrib.auth.views import LogoutView
 urlpatterns = [path('',ListaTareas.as_view(), name='tareas'),
+               path('login/', Login.as_view(), name='login'),
+               path('logout/',LogoutView.as_view(next_page='login'), name='logout'),
                path('crear-tarea/',CrearTarea.as_view(), name='crear-tarea'),
                path('editar-tarea/<int:pk>',EditarTarea.as_view(),name='editar-tarea'),
                path('eliminar-tarea/<int:pk>', eliminarTarea,name='eliminar-tarea'),]
--- base/views.py
+++ base/views.py
@@ -2,14 +2,30 @@ from django.contrib import messages
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from base.models import Tarea
 
+# LoginRequiredMixin: protege las vistas basadas en clases (CBV)
+# restringiendo el acceso solo a usuarios autenticados
 
 # Create your views here.
+class Login(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    # reescribir un metodo, una vez logueado
+    # te manda a la pagina principal de tareas
+    def get_success_url(self):
+        return reverse_lazy('tareas')
+
+
+#class Logout(LogoutView):
 
 # Vista tareas
-class ListaTareas(ListView):
+class ListaTareas(LoginRequiredMixin,ListView):
     model = Tarea
     context_object_name = 'tareas'
 
