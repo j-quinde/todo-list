c63eb83e7c55ca1e3f2ebc90df5cca2a516c66ff
Jossy Quinde <jqquindem@gmail.com>
2026-03-10 15:34:34 -0500

    style: estilos a la lista de tareas

--- base/static/base/js/script.js
+++ base/static/base/js/script.js
@@ -16,3 +16,22 @@ function confirm_delete(tarea_id) {
         }
     });
 }
+
+const tabs = document.querySelectorAll('.tabs a');
+const tab_contents = document.querySelectorAll('.tab-content');
+
+tabs.forEach(tab => {
+   tab.onclick = (e) => {
+       e.preventDefault();
+       const target = tab.getAttribute('href');
+
+       //quitar active
+       tabs.forEach(t => t.classList.remove('active'));
+       tab_contents.forEach(tc => tc.classList.remove('active'));
+
+       //agregar active
+       tab.classList.add('active');
+       document.querySelector(target).classList.add('active');
+   }
+});
+
